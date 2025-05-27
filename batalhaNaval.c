fe64fe79ebdce8542ad8fd82221986ccbb4f93ae
Passilongo <44105387+Passilongo@users.noreply.github.com>
2025-05-27 19:55:41 +0000

    Nivel iniciante feito

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,6 +10,54 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
+    char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    int linha[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int tabuleiro[10][10] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 3, 3, 3, 0, 0},
+        {0, 0, 3, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 3, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 3, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+
+    printf("***** TABULEIRO BATALHA NAVAL ***** \n\n");
+    printf("  ");
+
+    
+
+    for (int j = 0; j < 10; j++){ //Colunas da Tabela
+        printf("  %c", coluna[j]);
+       
+    }
+    printf("\n");
+
+    // Linhas da tabela
+
+    /*for (int i = 0; i < 10; i++){
+        printf(" %d \n", linha[i]);
+    }*/
+
+
+    // Tabela
+    for (int i = 0; i < 10; i++){   //linhas da tabela 
+        printf(" %d ", linha[i]);
+        for (int j = 0; j < 10; j++){   // Casas do tabuleiro
+            printf(" %d ", tabuleiro[i][j]);
+        }
+            printf("\n");
+    }
+    
+
+
+
+
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
