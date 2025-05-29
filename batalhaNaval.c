88c4bf8720c45d23a6634f43348e50f43948f680
Passilongo <44105387+Passilongo@users.noreply.github.com>
2025-05-29 01:11:50 +0000

    atualizando codigo diagonal inversa

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
-#define LINHAS = 10
-#define COLUNAS = 10
+#define LINHAS  10
+#define COLUNAS  10
 
 int main(){
 
     char coluna[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
     int linha[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int inicioDiag = 3;
+    int inicioDiagInversa = 6;
 
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    };
+    int tabuleiro[LINHAS][COLUNAS] = {0};
 
    /* tabuleiro [3][4] = 3;
     tabuleiro [3][5] = 3;
@@ -32,13 +23,28 @@ int main(){
     printf("***** TABULEIRO BATALHA NAVAL ***** \n\n");
     printf("  ");
 
-    
-
     for (int j = 0; j < 10; j++){ //Colunas da Tabela
         printf("  %c", coluna[j]);
        
     }
     printf("\n");
+
+    for (int i = 0; i < 3; i++){ //imprimindo 3 casas no tabuleiro Diagonal
+                
+        tabuleiro[inicioDiag + i][inicioDiag + i] = 3;
+           
+                /* if (i == j || i + j == 9){ // Diagonal e diagonal
+                tabuleiro[i][j] = 3;
+            }else {
+                tabuleiro[i][j] = 0;
+                }*/
+        }
+
+        for(int j = 0; j < 3; j++){
+                int l = inicioDiagInversa + j;
+                int c = (COLUNAS - 1) - (inicioDiagInversa + j); // Diagonal inversa
+                tabuleiro[l][c] = 3;
+            }
 
     // Linhas da tabela
 
@@ -53,14 +59,9 @@ int main(){
         for (int j = 0; j < 10; j++){   // Casas do tabuleiro
                   
                
-            for (int i = 0; i < 3; i++){ //imprimindo 3 casas no tabuleiro
-            if (i == j || i + j == 9){ // Diagonal e diagonal
-                tabuleiro[i][j] = 3;
-            }else {
-                tabuleiro[i][j] = 0;
-                }
-            }
+            
 
+            
           
             
             printf(" %d ", tabuleiro[i][j]);
